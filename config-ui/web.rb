2643dc5baa4458bc8998219afac08c251c985adb
Jon Wood <jon@blankpad.net>
2016-11-10 23:28:31 +0000

    Turns out I can use the built in account linking

--- config-ui/web.rb
+++ config-ui/web.rb
@@ -1,22 +1,10 @@
-require 'aws-sdk-core'
 require 'sinatra'
 
 get '/' do
-  erb :index, locals: { alexa_id: params['alexa_id'] }
+  erb :index, locals: { params: params }
 end
 
 post '/' do
-  if params['adafruit_key'].length > 100
-    status 422
-    body 'No one has an Adafruit key that long'
-  else
-    s3 = Aws::S3::Client.new
-    s3.put_object(
-      bucket: ENV['S3_BUCKET'],
-      key: params['alexa_id'],
-      acl: 'private',
-      body: params['adafruit_key']
-    )
-    "Done. Key: #{params['adafruit_key']}, Alexa ID: #{params['alexa_id']}"
-  end
+  final_uri = "#{params['redirect_uri']}#state=#{params['state']}&token_type=Bearer&access_token=#{params['adafruit_key']}" # rubocop:disable Metrics/LineLength
+  redirect to(final_uri)
 end
